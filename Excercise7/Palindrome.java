c001f44787541eb8ac0b567e68b3984a92d36b07
JimmySmuthers <vdegou@gmail.com>
2015-03-01 16:19:31 +0100

    Update Palindrome.java

--- Excercise7/Palindrome.java
+++ Excercise7/Palindrome.java
@@ -1,28 +1,34 @@
 package lists.doublylinked;
 
-import java.util.Scanner;
-
 public class Palindrome extends DList<Character> {
 
 	DList<Character> wordList = new DList<Character> ();
 
 	public Palindrome (String word){
-		String wordLower = word.toLowerCase(); // Convert the word to lower case in order to not run into case problems.
+		String wordLower = word.toLowerCase(); // Convert the structure to lower case in order to not run into case problems.
 		int characters = word.length();
 		for (int ii = 0; ii < characters; ii++) {
 			wordList.addLast(wordLower.charAt(ii));
 		}
 	}
 
+	/**
+	 * Checks if structure is a palindrome. Also works for sentences, so long as words are separated by a space.
+	 * @return True if the structure is a palindrome. False and an error message detailing the position of the discrepancy if not.
+	 */
 	public boolean isPalindrome() {
 		String forward = "";
 		String backward = "";
 		int index = 0;
 		DNode<Character> travelerNodeF = wordList.header.nextNode;
 		DNode<Character> travelerNodeB = wordList.trailer.previousNode;
-		while (forward.equals(backward) && index < wordList.getSize()) {
+		while (index < wordList.getSize()) {
+			if (travelerNodeF.getElement() != ' '){
 			forward = forward + travelerNodeF.getElement();
+			}
+			if (travelerNodeB.getElement() != ' '){
 			backward = backward + travelerNodeB.getElement();
+			}
 			travelerNodeF = travelerNodeF.getNextNode();
 			travelerNodeB = travelerNodeB.getPreviousNode();
 			index++;
@@ -31,15 +37,6 @@ public class Palindrome extends DList<Character> {
 			System.out.println("Discrepancy found at index " + index + ", printing last step: " + "\n" +  "Forward: " + forward + "\n" + "Backward: " + backward);
 			return false;
 		}
-		System.out.println("The word is a palindrome.");
+		System.out.println("The word(s) is a palindrome.");
 		return true;
 	}
-
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please introduce the word you would like to check.");
-		Palindrome word = new Palindrome(input.next());
-		word.isPalindrome();
-		input.close();
-	}
-}
