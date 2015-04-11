7b5e14cd54fef8912802049846d0da1f05918e19
JimmySmuthers <vdegou@gmail.com>
2015-04-11 18:30:01 +0200

    Create Test.java

---
+++ Exercise14/Test.java
@@ -0,0 +1,9 @@
+package fileBrowser;
+
+public class Test {
+
+	public static void main (String[] args){
+		FileBrowser a = new FileBrowser();
+		System.out.println(a.returnAll("D:\\Airdroid"));
+	}
+}
